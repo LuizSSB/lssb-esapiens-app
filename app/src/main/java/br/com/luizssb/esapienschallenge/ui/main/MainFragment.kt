034e4e9e0c3dbd1ea96cb41b451b1b616fe4bea1
Luiz Soares dos Santos Baglie <luizssb.biz@gmail.com>
2019-02-01 16:54:02 -0200

    Basic structure for handling people loading.

--- app/src/main/java/br/com/luizssb/esapienschallenge/ui/main/MainFragment.kt
+++ app/src/main/java/br/com/luizssb/esapienschallenge/ui/main/MainFragment.kt
@@ -6,10 +6,9 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import br.com.luizssb.esapienschallenge.R
 import br.com.luizssb.esapienschallenge.dependencies.viewModel
-import kotlinx.android.synthetic.main.main_fragment.view.*
+import kotlinx.android.synthetic.main.main_fragment.*
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.support.closestKodein
 
@@ -24,31 +23,42 @@ class MainFragment : Fragment(), KodeinAware {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val rootView = inflater
+    ): View = inflater
             .inflate(R.layout.main_fragment, container, false)
 
-        with(rootView) {
-            viewModel.people
-                .observe(this@MainFragment, Observer {
-                    grid_people.adapter = PeopleAdapter(context, it)
-                })
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-            viewModel.queryError
-                .observe(this@MainFragment, Observer {
-                    if (it !== null) {
-                        // Luiz: given the limited scope of the REST API (there
-                        // isn't even any kind of error response), if an error
-                        // occurs, it must be either unreachable network or
-                        // deserialization error. Since the latter is extremely
-                        // unlikely, we just assume the network failed and
-                        // call it a day.
-                        Toast.makeText(context, getString(R.string.error_query), Toast.LENGTH_SHORT).show()
-                    }
-                })
+        refresh_control.isRefreshing = true
+        refresh_control.setOnRefreshListener {
+            refresh_control.isRefreshing = true
+            viewModel.refresh()
         }
 
+        viewModel.people
+            .observe(this@MainFragment, Observer {
+                grid_people.adapter = PeopleAdapter(context!!, it)
+                finishLoading()
+            })
 
-        return rootView
+        viewModel.queryError
+            .observe(this@MainFragment, Observer {
+                if (it !== null) {
+                    // Luiz: given the limited scope of the REST API (there
+                    // isn't even any kind of error response), if an error
+                    // occurs, it must be either unreachable network or
+                    // deserialization error. Since the latter is extremely
+                    // unlikely, we just assume the network failed and
+                    // call it a day.
+                    finishLoading(getString(R.string.error_query))
+                }
+            })
+    }
+
+    private fun finishLoading(errorMsg: String? = null) {
+        refresh_control.isRefreshing = false
+        label_error.text = errorMsg ?: ""
+        container_error.visibility =
+            if (errorMsg === null) View.GONE else View.VISIBLE
     }
 }
